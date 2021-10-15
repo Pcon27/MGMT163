763bc0c70063a0cc56461c14d11b2f5b0ae8e89d
pconsag <pconsagra@g.ucla.edu>
2021-10-15 15:53:03 -0700

    add initial files:

---
+++ src/components/StudentContainer.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+import Footer from './footer'
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange}/>
+      {renderPage()}
+      <Footer/>
+    </div>
+  );
+}
